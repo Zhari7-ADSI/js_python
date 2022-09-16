e75b4d07406855b1586fe0598dd55b4862bcb82b
Zhari7 <meneseszharick29@gmail.com>
2022-09-16 17:16:48 -0500

    ejer_11

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
+let pi = Math.PI;
 let a = parseFloat(prompt("Ingrese el número de la a"));
 let b = parseFloat(prompt("Ingrese el número de la b"));
 let c = parseFloat(prompt("Ingrese el número de la c"));
-const x = 7;
 
-let oper = a * Math.pow(x,2) +b * x + c;
+let vol = (4/3) *pi * a * b * c;
 
-document.getElementById('oper').innerHTML=oper;
+document.getElementById('vol').innerHTML=vol;
