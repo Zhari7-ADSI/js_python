5df4a3d89946c8f19162c82067b8767abce56410
Zhari7 <meneseszharick29@gmail.com>
2022-09-16 23:00:33 -0500

    ejer_20

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
-let nom = prompt("Ingrese el nombre del empleado ");
-let hrsTrab = parseInt(prompt("Ingrese las horas trabajadas "));
-let hora = 15300;
+let vent1 = parseInt(prompt("Ingrese el valor de la venta 1"));
+let vent2 = parseInt(prompt("Ingrese el valor de la venta 2"));
+let vent3 = parseInt(prompt("Ingrese el valor de la venta 3"));
+let sueld = parseInt(prompt("Ingrese su sueldo"));
 
-let pago = hrsTrab * hora;
+let iva = (vent1 + vent2 + vent3) *0.10;
+let sueldo = iva + vent1 + vent2 + vent3 + sueld;
 
-document.getElementById('pago').innerHTML=pago;
-document.getElementById('nom').innerHTML=nom;
-document.getElementById('hrsTrab').innerHTML=hrsTrab;
+document.getElementById('iva').innerHTML=iva;
+document.getElementById('sueldo').innerHTML=sueldo;
